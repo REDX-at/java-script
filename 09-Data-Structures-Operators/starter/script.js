0608fc6e918ca75829135f1e4fcf8324d37050aa
Redx-at <taoussi.aimen@gmail.com>
2024-11-03 16:57:49 +0100

    Las push

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -143,22 +143,127 @@
 // // console.log(team1, team2, draw);
 // // console.log(players1Final);
 
-
-
-
 // hoisting
 
+// aimen3();
 
-aimen3();
+// function aimen3() {
+//   console.log('yes');
+// }
 
-function aimen3() {
-  console.log('yes');
+// const x = 10;
+// const y = 20;
+
+// [x, y] = [y, x];
+
+// const arrays = [1, 2, 3, 4, 5];
+// console.log(arrays.forEach());
+// console.log(arrays);
+// const secondearray = [];
+
+// let i = 0;
+// for (const item of arrays.entries()) {
+//   console.log(item);
+// secondearray[i] = item;
+// i++;
+// }
+
+// console.log(secondearray);
+
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// let i = 1;
+// for (const item of game.scored) {
+//   console.log('Goal ' + i + ' ' + item);
+//   i++;
+// }
+
+// const scores = {
+//   Gnabry: 1,
+//   Hummels: 1,
+//   Lewandowski: 1,
+// };
+
+// const team1 = game.odds.team1;
+// const draw = game.odds.x;
+// const team2 = game.odds.team2;
+
+// console.log('Odd of victory ' + game.team1 + ': ' + team1);
+// console.log('Odd of victory ' + 'draw: ' + draw);
+// console.log('Odd of victory ' + game.team2 + ': ' + team2);
+
+// console.log(scores);
+
+// const mapi = new Map();
+
+// mapi.set('name', 'Aimen').set('Age', 22).set('school', 1337);
+// mapi.delete('Age');
+// console.log(mapi.get('name'));
+// console.log(mapi.get('Age'));
+// console.log(mapi.size);
+
+// const test = '      aimen   gfgvg   \n'.trim();
+
+// console.log(test);
+// // const testa = test.trim
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const firstsplit = flights.split('+');
+
+// console.log(firstsplit);
+
+const first_one = firstsplit[0];
+const seonde_one = firstsplit[1];
+const third_one = firstsplit[2];
+const last_one = firstsplit[3];
+// console.log(first_one);
+// console.log(seonde_one);
+// console.log(third_one);
+// console.log(last_one);
+
+function fixstring(str) {
+  console.log(str);
 }
 
-
-const x = 10;
-const y = 20;
-
-
-[x, y] = [y, x];
-
+fixstring(first_one);
--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const flight = 'LH234';
+const redx = {
+  name: 'redx',
+  passport: 1937,
+};
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH789';
+  passenger.name = 'aimen ' + passenger.name;
+};
+checkIn(flight, redx);
+
+console.log(flight);
+console.log(redx);
