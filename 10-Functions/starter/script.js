f66756cf67ad8d3f3ab85f8e531959de6f64f51d
Redx-at <taoussi.aimen@gmail.com>
2024-11-03 19:58:06 +0100

    Project

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,15 +1,96 @@
 'use strict';
 
-const flight = 'LH234';
-const redx = {
-  name: 'redx',
-  passport: 1937,
-};
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH789';
-  passenger.name = 'aimen ' + passenger.name;
-};
-checkIn(flight, redx);
+// const flight = 'LH234';
+// const redx = {
+//   name: 'redx',
+//   passport: 1937,
+// };
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH789';
+//   passenger.name = 'aimen ' + passenger.name;
+// };
+// checkIn(flight, redx);
 
-console.log(flight);
-console.log(redx);
+// console.log(flight);
+// console.log(redx);
+
+// const runonce = function () {
+//   console.log('Im here for the first time');
+// };
+
+// runonce();
+
+// (function () {
+//   console.log('call method');
+// })();
+
+// (() => console.log(console.log('test')))();
+
+// const aimen = name => console.log(name);
+
+// aimen('redx');
+
+// const answer_pool = document.querySelector('.poll');
+
+// console.log(answer_pool.textContent);
+
+// function displayansr() {
+//   this.textContent = 'Smiti aimen';
+//   console.log(this.textContent);
+// }
+// answer_pool.addEventListener('click', displayansr);
+
+// {
+// }
+
+// test drari
+// const functionParent = function () {
+//   var varibincre = 0;
+//   var varibincre2 = 1;
+
+//   return function () {
+//     varibincre++;
+//     varibincre2++;
+//     console.log(`${varibincre} test`);
+//     console.log(`${varibincre2} test`);
+//   };
+// };
+
+// const functionchidle = functionParent();
+
+// functionchidle();
+// functionchidle();
+// functionchidle();
+
+let f;
+
+const g = function () {
+  const a = 2;
+
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+g();
+f();
+
+const d = function () {
+  const a = 4;
+
+  // f = function () {
+  //   console.log(a * 2);
+  // };
+  f = 3;
+};
+
+d();
+console.log(f);
+
+const person = {
+  name: 'Aimen',
+  age: 28,
+  hobbies: ['coding', 'designing'],
+};
+
+console.dir(typeof f);
